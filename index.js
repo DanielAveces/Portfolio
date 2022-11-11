05031511119e2ec0b4171a91aa47a217e3aedc5e
DanielAveces <102838405+DanielAveces@users.noreply.github.com>
2022-11-11 08:53:20 -0600

    V1.0
    First draw

---
+++ index.js
@@ -0,0 +1,25 @@
+$("#language").on("click", function() {
+  if ($(this).attr("src") === "images/US-UK_Flag.svg") {
+    $(this).attr("src", "images/mexico.svg");
+  } else if ($(this).attr("src") === "images/mexico.svg") {
+    $(this).attr("src", "images/US-UK_Flag.svg");
+  }
+});
+
+//URL for social media
+
+$(".fb").on("click", function() {
+  window.open("https://www.facebook.com/daniel.aveces/");
+});
+
+$(".twitter").on("click", function() {
+  window.open("https://twitter.com/Daniel_Acvs");
+});
+
+$(".linkedin").on("click", function() {
+  window.open("https://www.linkedin.com/in/juandanielaceves/");
+});
+
+$(".github").on("click", function() {
+  window.open("https://github.com/DanielAveces");
+});
